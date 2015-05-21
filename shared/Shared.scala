aae3aec227c5aca10b2ee48fe09f43687c410e77
Mathieu Leclaire <mathieu.leclaire@iscpif.fr>
2015-05-21 11:23:10 +0200

    Ugrade to scala 2.11.6, scala-js 0.6.3. Remove jsManager.

--- shared/Shared.scala
+++ shared/Shared.scala
@@ -1,13 +1,7 @@
 package shared
 
-import scala.scalajs.js.annotation.JSExport
 
-
-@JSExport
-case class MyCaseClass(hello: String)
 
 trait Api {
-  def hello(a: Int): Int
-  def caseClass(): MyCaseClass
   def uuid(): String = java.util.UUID.randomUUID.toString
 }
